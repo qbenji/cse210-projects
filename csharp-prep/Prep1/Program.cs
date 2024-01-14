d694a500cc659fa3ff7abfc095b93feccabee14e
Ben Quiñones <qbenji99@gmail.com>
2024-01-13 17:44:04 -0700

    Completed Prep1

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.Write("What is your first name? ");
+        string NameFirst = Console.ReadLine();
+
+        Console.Write("What is your last name? ");
+        string NameLast = Console.ReadLine();
+        
+        Console.WriteLine($"\nYour name is {NameLast}, {NameFirst} {NameLast}.");
     }
 }
