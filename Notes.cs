45aaf8b1d5dac3f751c35590edce64e7a9e3db88
Ben Quiñones <qbenji99@gmail.com>
2024-01-18 13:23:03 -0700

    Added more to notes

--- Notes.cs
+++ Notes.cs
@@ -78,3 +78,42 @@ int number = int.Parse(userInput);
 
 int number = 42;
 string textVersion = number.ToString();
+
+// While Loops
+string response = "yes";
+
+while (response == "yes")
+{
+    Console.Write("Do you want to continue? ");
+    response = Console.ReadLine();
+}
+
+// Do-While Loops
+string response;
+
+do
+{
+    Console.Write("Do you want to continue? ");
+    response = Console.ReadLine();
+} while (response == "yes");
+
+// For Loops
+for (int i = 0; i < 10; i++)
+{
+    Console.WriteLine(i);
+}
+
+for (int i = 2; i <= 20; i = i + 2)
+{
+    Console.WriteLine(i);
+}
+
+// Foreach Loops
+foreach (string color in colors)
+{
+    Console.WriteLine(color);
+}
+
+// Random Numbers
+Random randomGenerator = new Random();
+int number = randomGenerator.Next(1, 11);
