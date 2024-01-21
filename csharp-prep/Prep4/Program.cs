03f9959c339ddc3565bd0ede1dc0dbdd87b88f5e
Ben Quiñones <qbenji99@gmail.com>
2024-01-20 21:37:03 -0700

    Enhanced core and completed stretch

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,13 +4,53 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        Console.WriteLine("Enter a list of integers (positive or negative), type 0 when finished.");
         List<int> numbers = new List<int>();
         int number = 1;
+    
+        while (number != 0)
+        {
+            Console.Write("Enter a number: ");
+            number = int.Parse(Console.ReadLine());
+            numbers.Add(number);
+        }
+        Console.WriteLine($"Sum: {numbers.Sum()}\nAverage: {numbers.Average()}\nMaximum: {numbers.Max()}");
+
+        // Stretch
+        numbers.Sort();
+        int leastPositive = 999999999;
+        foreach (int num in numbers)
+        {
+            Console.WriteLine(num);
+            if (leastPositive > num & num >= 0)
+            {
+                leastPositive = num;
+            }
+        }
+        Console.WriteLine($"Least Positive: {leastPositive}");
+        
+        /* 👇 Alternate code to meet core 👇
         int numCount = 0;
         int sum = 0;
         int max = -999999999;
         int avg = 0;
+
+        foreach (int num in numbers)
+        {
+            numCount += 1;
+            sum += number;
+            if (max < number)
+            {
+                max = number;
+            }
+        }
+        Console.WriteLine($"Sum: {sum}\nAverage: {sum/numCount}\nMaximum: {max}");
+        // ---------------------------------------------------------------------------
+        int numCount = 0;
+        int sum = 0;
+        int max = -999999999;
+        int avg = 0;
+
         while (number != 0)
         {
             numCount += 1;
@@ -24,10 +64,6 @@ class Program
             }
         }
         Console.WriteLine($"Sum: {sum}\nAverage: {sum/numCount}\nMaximum: {max}");
-
-        /*
-        👇 Just to demonstrate I know how to use "for" loops to do the same thing I did in the while loop 👇
-
-        */
+        //*/
     }
 }
