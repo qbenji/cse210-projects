b1289e4a6ebcaae343089d36ffca13f693bc725b
Ben Quiñones <qbenji99@gmail.com>
2024-01-20 21:00:28 -0700

    Began of Preg4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,14 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        List<int> numbers = new List<int>();
+        int number = 1;
+        while (number != 0)
+        {
+            Console.Write("Enter a number: ");
+            number = int.Parse(Console.ReadLine());
+            numbers.Add(number);
+        }
     }
 }
