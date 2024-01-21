32a57341faaa314165e827ff429db9ba1910da8f
Ben Quiñones <qbenji99@gmail.com>
2024-01-20 22:31:25 -0700

    Completed the core. Nothing more done since no stretch and am quite tired.

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcome();
+        string name = PromptUserName();
+        int num = PromptUserNumber();
+        int math = SquareNumber(num);
+        DisplayResult(name,math);
+    }
+
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the program!");
+    }
+
+    static string PromptUserName()
+    {
+        Console.Write("Please enter your name: ");
+        string name = Console.ReadLine();
+
+        return name;
+    }
+
+    static int PromptUserNumber()
+    {
+        Console.Write("Please enter your favorite number: ");
+        int num = int.Parse(Console.ReadLine());
+
+        return num;
+    }
+
+    static int SquareNumber(int num)
+    {
+        int math = num * num;
+        return math;
+    }
+
+    static void DisplayResult(string name, int num)
+    {
+        Console.WriteLine($"{name}, the square of your number is {num}");
     }
 }
