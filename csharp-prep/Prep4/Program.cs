155eb95c72ccd21155addb3377745bae07e1ef61
Ben Quiñones <qbenji99@gmail.com>
2024-01-20 21:15:21 -0700

    Finished Core

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -7,11 +7,27 @@ class Program
         Console.WriteLine("Enter a list of numbers, type 0 when finished.");
         List<int> numbers = new List<int>();
         int number = 1;
+        int numCount = 0;
+        int sum = 0;
+        int max = -999999999;
+        int avg = 0;
         while (number != 0)
         {
+            numCount += 1;
             Console.Write("Enter a number: ");
             number = int.Parse(Console.ReadLine());
             numbers.Add(number);
+            sum += number;
+            if (max < number)
+            {
+                max = number;
+            }
         }
+        Console.WriteLine($"Sum: {sum}\nAverage: {sum/numCount}\nMaximum: {max}");
+
+        /*
+        👇 Just to demonstrate I know how to use "for" loops to do the same thing I did in the while loop 👇
+
+        */
     }
 }
