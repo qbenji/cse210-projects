76666d4b26b10119252c8f4174c83f92dffe210b
Ben Quiñones <qbenji99@gmail.com>
2024-01-20 20:43:49 -0700

    Finished guessing game. Accomplished stretch challenge and went beyond it.

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,28 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.Write("What is the magic number? (Integers Only) ");
-        int magicNum = int.Parse(Console.ReadLine());
-        int guessNum = 0;
+        Random randomGenerator = new Random();
+        Console.WriteLine("Welcome to Ben's Guessing Game! A random integer will be generated, and it is your task to guess that number in the least amount of tries!");
+        Console.Write("Would you like to play? Yes (y) or No (n) ");
+        string playing = Console.ReadLine();
 
-        do
-        {
-            Console.Write("\nWhat is your guess? (Integers Only) ");
-            guessNum = int.Parse(Console.ReadLine());
+        while (playing == "y")
+        {   
+            int rangeNum = randomGenerator.Next(10, 100);
+            int magicNum = randomGenerator.Next(1, rangeNum);
+            Console.WriteLine($"Guess a number between 1 & {rangeNum}");
+            int guessNum = 0;
+            int guessCount = 0;
 
-            if (guessNum < magicNum)
+            do
             {
-                Console.WriteLine("Higher");
-            }
-            else if (guessNum > magicNum)
-            {
-                Console.WriteLine("Lower");
-            }
-            else
-            {
-                Console.WriteLine("You guessed it!");
-            }     
-        } while (guessNum != magicNum);
+                Console.Write("\nWhat is your guess? (Integers Only) ");
+                guessNum = int.Parse(Console.ReadLine());
+                guessCount += 1;
 
+                if (guessNum < magicNum)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else if (guessNum > magicNum)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else
+                {
+                    Console.WriteLine($"You guessed it! It took you {guessCount} tries.");
+                }     
+            } while (guessNum != magicNum);
+
+            Console.Write("\nWould you like to play again? Yes (y) or No (n) ");
+            playing = Console.ReadLine();
+        }
+        
     }
 } 
