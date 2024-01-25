8a4b2104121d7d8dfa830486a91647ee0741c486
Ben Quiñones <qbenji99@gmail.com>
2024-01-24 20:21:14 -0700

    Created Jobs.cs, Resume.cs, & finished Learning02

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        Job job2 = new Job();
+        Resume myResume = new Resume();
+
+        myResume._name = "Allison Rose";
+        job1._jobTitle = "Software Engineer";   job1._company = "Microsoft";    job1._startYear = 2019;     job1._endYear = 2022;
+        job2._jobTitle = "Manager";             job2._company = "Apple";        job2._startYear = 2022;     job2._endYear = 2023;
+        
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
     }
 }
