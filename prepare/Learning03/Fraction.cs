1995677438012b0086a2853ef6a73e15750e2486
Ben Quiñones <qbenji99@gmail.com>
2024-02-06 21:57:39 -0700

    Continued progress on Fractions class

--- prepare/Learning03/Fraction.cs
+++ prepare/Learning03/Fraction.cs
@@ -7,42 +7,45 @@ public class Fraction
     
     public Fraction()
     {
-
+        _top = 1;
+        _bottom = 1;
     }
 
     public Fraction(int wholeNumber)
     {
-        
+        _top = wholeNumber;
+        _bottom = 1;
     }
 
     public Fraction(int top, int bottom)
     {
-        
+        _top = top;
+        _bottom = bottom;
     }
 
     public void GetTop()
     {
-
+        Console.WriteLine(_top);
     }
 
     public void SetTop(int top)
     {
-
+        _top = top;
     }
 
     public void GetBottom()
     {
-
+        Console.WriteLine(_bottom);
     }
 
     public void SetBottom(int bottom)
     {
-
+        _bottom = bottom;
     }
 
     public void GetFraction()
-    {
-
+    {   
+        
     }
 
     public void GetDecimalValue()
