3b9e7adacea3b0783f3cee4f1d24c4de688f78b8
Ben Quiñones <qbenji99@gmail.com>
2024-02-17 13:50:35 -0700

    Worked on the Scripture class and did some testing in main file

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -4,7 +4,7 @@ class Program
 {
     static void Main(string[] args)
     {
-        Reference _ref = new Reference("Proverbs", "3", "5", "6");
-        Console.WriteLine($"{_ref.GetReference()}\n");
+        Reference _ref = new Reference();
+        Console.WriteLine($"{_ref.GetReference()}");
     }
 }
--- prove/Develop03/Scripture.cs
+++ prove/Develop03/Scripture.cs
@@ -1,9 +1,18 @@
 public class Scripture
 {
-    Reference _reference = new Reference();
-    public List<Word> _words = new List<Word>();
+    private string _scripture;
     public Scripture()
     {
-        
+        _scripture = "Trust in the Lord with all thine heart; and lean not unto thine own understanding. In all thy ways acknowledge him, and he shall direct thy paths.";
+    }
+
+    public Scripture(string scripture)
+    {
+        _scripture = scripture;
+    }
+
+    public string GetScripture()
+    {
+        return _scripture;
     }
 }
