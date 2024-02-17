2b85fa783c2e22cd1d5c040668f4787f66399089
Ben Quiñones <qbenji99@gmail.com>
2024-02-17 15:24:55 -0700

    Progress Update

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -4,8 +4,33 @@ class Program
 {
     static void Main(string[] args)
     {
+
+        Console.WriteLine("Welcome to the Scripture Memorizer!\n");
+
         Reference _ref = new Reference();
         Scripture _scrip = new Scripture();
-        Console.Write($"{_ref.GetReference()} {_scrip.GetScripture()} ");
+        string choice = "";
+        int index = 0;
+        string scripture = _scrip.GetScripture();
+        string reference = _ref.GetReference();
+        string[] words = _scrip.GetScripture().Split(" ");
+
+        Console.WriteLine(scripture);
+        Console.WriteLine(reference);
+
+        Console.WriteLine("\nPress enter to continue or type 'quit' to exit: ");
+        choice = Console.ReadLine();
+
+        while (choice != "quit")
+        {
+            foreach (string word in words)
+            {
+                Word w = new Word(word);
+                w.HideWord();
+                words[index] = w.GetWord();
+                Console.Write($"{words[index]} ");
+                index += 1;
+            }
+        }
     }   
 }
