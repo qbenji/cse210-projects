246a493f9bff701a6cbfa8d749b3aeb1b10ec373
Ben Quiñones <qbenji99@gmail.com>
2024-02-17 14:33:40 -0700

    Made significant progress on Word.cs

--- prove/Develop03/Word.cs
+++ prove/Develop03/Word.cs
@@ -1,18 +1,46 @@
 using System;
+using System.Collections.Generic;
 
 public class Word
 {
     private string _word;
-    private List<char> _letters;
+    private string _backupWord;
+    private List<char> _letters = new List<char>();
     private bool _hidden = false;
     public Word(string word)
     {
         _word = word;
+        _backupWord = word;
+        foreach (char letter in _word)
+        {
+            _letters.Add(letter);
+        }
     }
 
     public void HideWord()
     {
         _hidden = true;
-        _letters = Word.ToList();
+        foreach (char letter in _word)
+        {
+            _letters.Add('_');
+        }
+        _word = new string(_letters.ToArray());
     }
+
+    public void ShowWord()
+    {
+        _hidden = false;
+        _word = _backupWord;
+    }
+
+    public bool HiddenCheck()
+    {
+        return _hidden;
+    }
+
+    public string GetWord()
+    {
+        return _word;
+    }
+
 }
