15fa22a4089f0d7d45c560edf4bef2617d0858e8
Ben Quiñones <qbenji99@gmail.com>
2024-02-17 14:35:17 -0700

    Entering testing

--- prove/Develop03/Scripture.cs
+++ prove/Develop03/Scripture.cs
@@ -3,7 +3,6 @@ using System;
 public class Scripture
 {
     private string _scripture;
-    private string[] _scriptureWords;
     public Scripture()
     {
         _scripture = "Trust in the Lord with all thine heart; and lean not unto thine own understanding. In all thy ways acknowledge him, and he shall direct thy paths.";
@@ -14,13 +13,8 @@ public class Scripture
         _scripture = scripture;
     }
 
-    public void GetScripture()
+    public string GetScripture()
     {
-        _scriptureWords = _scripture.Split(" ");
-        
-        foreach (string word in _scriptureWords)
-        {
-            Console.Write($"{word} ");
-        }
+        return _scripture;
     }
 }
