a0ef5313386a934506f3ce0f6a9be02c58d9d320
Ben Quiñones <qbenji99@gmail.com>
2024-02-27 13:23:49 -0700

    Finished the bonus generator classes

--- prove/Develop04/ListingPromptGenerator.cs
+++ prove/Develop04/ListingPromptGenerator.cs
@@ -1,4 +1,18 @@
 public class ListingPromptGenerator
 {
-    
+    public string[] _prompts = new string[]
+    {
+        "Who are people that you appreciate?",
+        "What are personal strengths of yours?",
+        "Who are people that you have helped this week?",
+        "When have you felt the Holy Ghost this month?",
+        "Who are some of your personal heroes?"
+    };
+
+    public string GetRandomPrompt()
+    {
+        Random random = new Random();
+        int index = random.Next(_prompts.Length);
+        return _prompts[index];
+    }
 }
