45494be3d75a140a4a3fc1c6efd63c81e97c4780
Ben Quiñones <qbenji99@gmail.com>
2024-02-29 13:20:04 -0700

    Added menu to Program.cs

--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -4,6 +4,33 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop04 World!");
+        int choice = 0;
+
+        while (choice != 4)
+        {   
+            Console.WriteLine("Welcome to the Mindfulness Program. Please select one of the following activities to begin a mindfulness session:");
+            Console.Write("\n  1. Breathing Activity\n  2. Reflection Activity\n  3. Lising Activity\n  4. Quit\n> ");
+            choice = int.Parse(Console.ReadLine());
+
+            switch (choice)
+            {
+                case 1: // Breathing Activity
+                    break;
+
+                case 2: // Reflection Activity
+                    break;
+
+                case 3: // Listing Activity
+                    break;
+
+                case 4: // Quit
+                    Console.WriteLine("\nYou are now exiting the program. Goodbye.");
+                    break;
+
+                default: // invalid choice
+                    Console.WriteLine("\nInvalid Choice, try again.");
+                    break;
+            }
+        }
     }
 }
