c5144e223b1c152650bef8098f77019037750785
Ben Quiñones <qbenji99@gmail.com>
2024-03-03 21:50:01 -0700

    Refined Breathing Activity

--- prove/Develop04/BreathingActivity.cs
+++ prove/Develop04/BreathingActivity.cs
@@ -15,5 +15,30 @@ public class BreathingActivity : Activity
         Console.WriteLine("Breath Out...");
         Timer(seconds);
     }
-    
+
+    public void Breathing()
+    {
+        int duration = GetDuration();
+        while (duration > 0)
+        {
+            BreathIn(4);
+            BreathOut(6);
+            duration -= 10;
+            Console.Clear();
+            if (duration < 10 && duration >= 7)
+            {
+                double number = duration/2;
+                int durationOne = (int)Math.Ceiling(number);
+                int durationTwo = (int)Math.Floor(number);
+                BreathIn(durationOne);
+                BreathOut(durationTwo);
+                duration -= duration;
+                Console.Clear();
+            }
+            else if (duration < 7 && duration > 0)
+            {
+                Spinner(duration);
+            }
+        }
+    }
 }
