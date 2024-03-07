6950927fb02abbdb7eeec1d363127b20e92f295e
Ben Quiñones <qbenji99@gmail.com>
2024-03-06 20:48:03 -0700

    Adjusted code placement

--- prepare/Learning05/Rectangle.cs
+++ prepare/Learning05/Rectangle.cs
@@ -3,8 +3,7 @@ public class Rectangle : Shape
     private double _length;                                         // initialize _length to blank
     private double _width;                                          // initialize _width to blank
 
-    public Rectangle(string color, double length, double width)     // constructor
-        : base (color)
+    public Rectangle(string color, double length, double width) : base (color)  // constructor
     {
         _length = length;
         _width = width;
