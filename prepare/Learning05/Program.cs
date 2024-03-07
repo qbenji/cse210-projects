f30ad94bc37a1ff97c1c2d52f3b3184ff3df73e3
Ben Quiñones <qbenji99@gmail.com>
2024-03-06 20:57:37 -0700

    Changed placement of code

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,18 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
-        List<Shape> shapes = new List<Shape>();         // create a list of shapes
+        List<Shape> shapes = new List<Shape>();                             // create a list of shapes
 
-        Square sq = new Square("Red", 3);               // create a red square of length 3
-        shapes.Add(sq);                                 // add the square to the list
+        Square sq = new Square("Red", 3);               shapes.Add(sq);     // create a red square of length 3 and add it to the list
+        Rectangle rec = new Rectangle("Blue", 4, 5);    shapes.Add(rec);    // create a blue rectangle of length 4 and width 5 and add it to the list
+        Circle cir = new Circle("Green", 6);            shapes.Add(cir);    // create a green circle of radius 6 and add it to the list
 
-        Rectangle rec = new Rectangle("Blue", 4, 5);    // create a blue rectangle of length 4 and width 5
-        shapes.Add(rec);                                // add the rectangle to the list
-
-        Circle cir = new Circle("Green", 6);            // create a green circle of radius 6
-        shapes.Add(cir);                                // add the circle to the list
-
-        foreach (Shape s in shapes)                     // loop through the list of shapes
+        foreach (Shape s in shapes)                                         // loop through the list of shapes and call the GetArea and GetColor methods on each shape and print each result
         {
             string color = s.GetColor();
             double area = s.GetArea();
