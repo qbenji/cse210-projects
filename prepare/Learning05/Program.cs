ea1b6ee3ce962997995a8ee7b3329c9c3d618ab9
Ben Quiñones <qbenji99@gmail.com>
2024-03-06 20:53:01 -0700

    Completed Program

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new List<Shape>();         // create a list of shapes
+
+        Square sq = new Square("Red", 3);               // create a red square of length 3
+        shapes.Add(sq);                                 // add the square to the list
+
+        Rectangle rec = new Rectangle("Blue", 4, 5);    // create a blue rectangle of length 4 and width 5
+        shapes.Add(rec);                                // add the rectangle to the list
+
+        Circle cir = new Circle("Green", 6);            // create a green circle of radius 6
+        shapes.Add(cir);                                // add the circle to the list
+
+        foreach (Shape s in shapes)                     // loop through the list of shapes
+        {
+            string color = s.GetColor();
+            double area = s.GetArea();
+
+            Console.WriteLine($"The {color} shape has an area of {area}");
+        }
     }
 }
