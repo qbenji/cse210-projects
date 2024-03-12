55a1a8982b4a779591c8cb422f3c7881316a1743
Ben Quiñones <qbenji99@gmail.com>
2024-03-12 13:17:47 -0600

    Created the menu

--- prove/Develop05/Program.cs
+++ prove/Develop05/Program.cs
@@ -4,6 +4,49 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("x");
+        Console.Clear();
+        int choice = 0;
+        int points = 0;
+        Console.Write($"You have {points} points.");
+        Console.ReadLine();
+        
+        while (choice != 4)
+        {   
+            Console.Clear();
+            Console.WriteLine("Menu Options:");
+            Console.Write("\n  1. Create New Goal\n  2. List Goals\n  3. Save Goals\n  4. Load Goals\n  5. Record Event\n  6. Quit\n> ");
+            choice = int.Parse(Console.ReadLine());
+
+            switch (choice)
+            {
+                case 1: // Create New Goal
+                    
+                    break;
+
+                case 2: // List Goals
+                    
+                    break;
+
+                case 3: // Save Goals
+                    
+                    break;
+
+                case 4: // Load Goals
+                    
+                    break;
+
+                case 5: // Record Event
+                    
+                    break;
+
+                case 6: // Quit
+                    
+                    break;
+
+                default: // Invalid Choice
+                    
+                    break;
+            }
+        }
     }
 }
