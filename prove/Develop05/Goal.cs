75554aeaeab0f467235c4a21afe091fc1faf0e56
Ben Quiñones <qbenji99@gmail.com>
2024-03-12 13:36:17 -0600

    Created getters

--- prove/Develop05/Goal.cs
+++ prove/Develop05/Goal.cs
@@ -4,19 +4,40 @@ public class Goal
     private string _goalDesc;       // goal description
     private int _goalNum;           // goal number
     private string _goalType;       // goal type
-    private int _points;            // points
-    public Goal(string goalName, string goalDesc, int goalNum, string goalType, int points) // constructor
+    private int _goalPoints;        // points
+    public Goal(string goalName, string goalDesc, int goalNum, string goalType, int goalPoints) // constructor
     {
         _goalName = goalName;
         _goalDesc = goalDesc;
         _goalNum = goalNum;
         _goalType = goalType;
-        _points = points;
+        _goalPoints = goalPoints;
     }
 
-    public string GetGoalName()
+    public string GetGoalName()     // get goal name
     {
         return _goalName;
     }
+
+    public string GetGoalDesc()     // get goal description
+    {
+        return _goalDesc;
+    }
+
+    public int GetGoalNum()         // get goal number
+    {
+        return _goalNum;
+    }
+
+    public string GetGoalType()     // get goal type
+    {
+        return _goalType;
+    }
+
+    public int GetGoalPoints()      // get goal points
+    {
+        return _goalPoints;
+    }
+
     
 }
