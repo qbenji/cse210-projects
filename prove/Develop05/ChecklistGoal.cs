73df1ed6a4d5997c0be0f15612cbd56ab6c83177
Ben Quiñones <qbenji99@gmail.com>
2024-03-12 13:25:03 -0600

    Fixed class name for Checklist Goal. Finished constructor and attributes of Goal parent class

--- prove/Develop05/ChecklistGoal.cs
+++ prove/Develop05/ChecklistGoal.cs
@@ -1,4 +1,4 @@
-public class Checklist : Goal
+public class ChecklistGoal : Goal
 {
 
 
--- prove/Develop05/Goal.cs
+++ prove/Develop05/Goal.cs
@@ -1,6 +1,22 @@
 public class Goal
 {
+    private string _goalName;       // goal name
+    private string _goalDesc;       // goal description
+    private int _goalNum;           // goal number
+    private string _goalType;       // goal type
+    private int _points;            // points
+    public Goal(string goalName, string goalDesc, int goalNum, string goalType, int points) // constructor
+    {
+        _goalName = goalName;
+        _goalDesc = goalDesc;
+        _goalNum = goalNum;
+        _goalType = goalType;
+        _points = points;
+    }
 
-
-
+    public string GetGoalName()
+    {
+        return _goalName;
+    }
+    
 }
