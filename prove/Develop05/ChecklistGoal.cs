77bc73735fb6ba60e31654e0a6b7b453dce426f2
Ben Quiñones <qbenji99@gmail.com>
2024-03-16 22:13:39 -0600

    Added getters and setters

--- prove/Develop05/ChecklistGoal.cs
+++ prove/Develop05/ChecklistGoal.cs
@@ -1,3 +1,5 @@
+using System.Runtime.CompilerServices;
+
 public class ChecklistGoal : Goal
 {
 
@@ -11,5 +13,26 @@ public class ChecklistGoal : Goal
         _bonusPoints = bonusPoints;
     }
 
+
+
+    public int GetCountToBonus()   // get count to bonus
+    {
+        return _countToBonus
+    }
+
+    public int GetBonusPoints()    // get bonus points
+    {
+        return _bonusPoints;
+    }
+
+    public void SetCountToBonus(int countToBonus)    // set count to bonus
+    {
+        _countToBonus = countToBonus;
+    }
+
+    public void SetBonusPoints(int bonusPoints)     // set bonus points
+    {
+        _bonusPoints = bonusPoints;
+    }
 
 }
