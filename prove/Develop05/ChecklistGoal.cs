e98d85efe49753d89dbe7aa21423b7721593b1b0
Ben Quiñones <qbenji99@gmail.com>
2024-03-16 22:09:29 -0600

    Added to the constructor

--- prove/Develop05/ChecklistGoal.cs
+++ prove/Develop05/ChecklistGoal.cs
@@ -1,8 +1,15 @@
 public class ChecklistGoal : Goal
 {
-    public ChecklistGoal(string goalName, string goalDesc, int goalNum, string goalType, int goalPoints)    // constructor
+
+    private int _countToBonus;    // count to bonus
+    private int _bonusPoints;     // bonus points
+
+    public ChecklistGoal(string goalName, string goalDesc, int goalNum, string goalType, int goalPoints, int bonusPoints, int countToBonus)    // constructor
         : base(goalName, goalDesc, goalNum, goalType, goalPoints) 
-    {}
+    {
+        _countToBonus = countToBonus;
+        _bonusPoints = bonusPoints;
+    }
 
 
 }
