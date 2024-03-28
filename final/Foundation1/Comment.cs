5a8db9549371517a41de0b1f061437fe39a89fe1
Ben Quiñones <qbenji99@gmail.com>
2024-03-28 12:22:25 -0600

    Started work on Foundational 1

---
+++ final/Foundation1/Comment.cs
@@ -0,0 +1,12 @@
+using System;
+
+public class Comment
+{
+    private string _name;
+    private string _comment;
+    public Comment(string name, string comment)
+    {
+        _name = name;
+        _comment = comment;
+    }
+}
