09e8edaa4d37352ca1604d98ee2f08013f13bdd3
Ben Quiñones <qbenji99@gmail.com>
2024-03-30 14:10:55 -0600

    Made progress on Order class

--- final/Foundation2/Order.cs
+++ final/Foundation2/Order.cs
@@ -2,5 +2,32 @@ using System;
 
 public class Order
 {
+    private Customer _customer;
+    private List<Products> _products;
 
+    public Order(Customer customer, List<Products> products)    // constructor
+    {
+        _customer = customer;
+        _products = products;
+    }
+
+    public double TotalCost()       // calculate total cost
+    {
+        double totalCost = 0;
+        foreach (Products product in _products)
+        {
+            totalCost += product.TotalPrice();
+        }
+
+        double shippingCost;
+        if (_customer.IsUSA())
+        {
+            shippingCost = 5;
+        }
+        else
+        {
+            shippingCost = 35;
+        }
+        return totalCost + shippingCost;
+    }
 }
