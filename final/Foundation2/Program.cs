d35437f986388e43ea154575a9a3b4ad93d39d8d
Ben Quiñones <qbenji99@gmail.com>
2024-03-30 14:25:27 -0600

    Completed initial build of program

--- final/Foundation2/Program.cs
+++ final/Foundation2/Program.cs
@@ -4,6 +4,18 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("");
+        Address a1 = new Address("525 S Center St", "Rexburg", "ID", "USA");
+        Customer c1 = new Customer("Ben Quiñones", a1);
+
+        List<Products> p1 = new List<Products>();
+        p1.Add(new Products("Trash Can", "T01", 19.99, 1));
+        p1.Add(new Products("Trash Bag", "B09", 7.99, 2));
+
+        Order o1 = new Order(c1, p1);
+
+        Console.WriteLine("Order 1 Summary...");
+        Console.WriteLine(o1.PackingLabel());
+        Console.WriteLine(o1.ShippingLabel());
+        Console.WriteLine("Total Cost: $" + o1.TotalCost());
     }
 }
