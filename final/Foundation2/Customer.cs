2594b57eeba8e2d15d2e005c7cae2cbed5f53492
Ben Quiñones <qbenji99@gmail.com>
2024-03-30 13:52:11 -0600

    Finished initial build of Customer class

--- final/Foundation2/Customer.cs
+++ final/Foundation2/Customer.cs
@@ -2,5 +2,37 @@ using System;
 
 public class Customer
 {
+    private string _name;
+    private Address _address;
 
+    public Customer(string name, Address address)       // constructor
+    {
+        _name = name;
+        _address = address;
+    }
+
+    public bool IsUSA()                                 // check if country is USA
+    {
+        return _address.IsUSA();
+    }
+
+    public string GetName()                             // get name
+    {
+        return _name;
+    }
+
+    public Address GetAddress()                         // get address
+    {
+        return _address;
+    }
+
+    public void SetName(string name)                    // set name
+    {
+        _name = name;
+    }
+
+    public void SetAddress(Address address)             // set address
+    {
+        _address = address;
+    }
 }
