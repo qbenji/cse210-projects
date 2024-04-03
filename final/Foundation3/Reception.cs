6791b2e9a003075604a3fab894bbf8ca5ac453c4
Ben Quiñones <qbenji99@gmail.com>
2024-04-03 10:38:18 -0600

    Finished initial build of Reception class

--- final/Foundation3/Reception.cs
+++ final/Foundation3/Reception.cs
@@ -2,5 +2,25 @@ using System;
 
 public class Reception : Event
 {
+    private string _rsvpEmail;
+    public Reception(string title, string description, DateTime date, TimeSpan time, Address address, string eventType, string rsvpEmail)
+        : base(title, description, date, time, address, eventType)
+    {
+        _rsvpEmail = rsvpEmail;
+    }
 
+    public override string FullDetails()
+    {
+        return $"{GetEventType()} - {GetTitle()}\n{GetDescription()}\n\n  {GetAddress()}\n   {GetDate()} @ {GetTime()}\n\nRSVP Email: {_rsvpEmail}";
+    }
+
+    public string GetRsvpEmail()
+    {
+        return _rsvpEmail;
+    }
+
+    public void SetRsvpEmail(string rsvpEmail)
+    {
+        _rsvpEmail = rsvpEmail;
+    }
 }
