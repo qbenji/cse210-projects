6b291dc3a1e853a71e7a8eea3b20d9773556bf24
Ben Quiñones <qbenji99@gmail.com>
2024-04-03 11:22:33 -0600

    Made significant progress on Program.cs

--- final/Foundation3/Program.cs
+++ final/Foundation3/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("");
+        // Addresses
+        Address a1 = new Address("525 S Center St", "Rexburg", "ID", "USA");
+        Address a2 = new Address("859 S Yellowstone Hwy #1002", "Rexburg", "ID", "USA");
+        Address a3 = new Address("S 2nd W & 3rd W", "Rexburg", "ID", "USA");
+
+        // Events
+        Event lecture = new Lecture("Programming with Classes", "Come sling code with Brother Thayne", DateTime.Now, new TimeSpan(16, 0, 0), a1, "Lecture", "Brother Thayne",48);
+        Event reception = new Reception("Ben and John's Reception", "Pride month is our favorite month", DateTime.Now, new TimeSpan(18, 0, 0), a2, "Reception","BeJo@gmail.com");
+        Event outdoorGathering = new OutdoorGathering("Soccer", "It's the best sport", DateTime.Now, new TimeSpan(18, 0, 0), a3, "Outdoor Gathering", 76, "Sunny", 40, 2, 1);
+        
     }
 }
