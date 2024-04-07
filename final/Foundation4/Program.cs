1e682a907b0ae0367e629a69cdb0e3040500cd73
Ben Quiñones <qbenji99@gmail.com>
2024-04-06 20:07:14 -0600

    Finished Program

--- final/Foundation4/Program.cs
+++ final/Foundation4/Program.cs
@@ -4,6 +4,18 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("");
+        // Activities
+        Activity running = new Running(DateTime.Now, 30, 1.5);
+        Activity cycling = new Cycling(DateTime.Now, 30, 15);
+        Activity swimming = new Swimming(DateTime.Now, 30, 3);
+
+        // Listify
+        Activity[] activities = { running, cycling, swimming };
+
+        // Summary
+        foreach (Activity activity in activities)
+        {
+            Console.WriteLine(activity.GetSummary());
+        }
     }
 }
