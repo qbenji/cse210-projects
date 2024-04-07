f52d87e02a5ee3661dad81edb7c47c899b24c0aa
Ben Quiñones <qbenji99@gmail.com>
2024-04-06 19:22:18 -0600

    Finished Cycling class

--- final/Foundation4/Cycling.cs
+++ final/Foundation4/Cycling.cs
@@ -2,5 +2,27 @@ using System;
 
 public class Cycling : Activity
 {
+    private double _speed;
+
+    public Cycling(DateTime date, int durationMin, double speed)     // constructor
+        : base(date, durationMin)
+    {
+        _speed = speed;
+    }
+
+    public override double GetDistance()                // get distance
+    {
+        return (_speed / 60) * GetDurationMin();
+    }
+
+    public override double GetSpeed()                   // get speed
+    {
+        return _speed;
+    }
+
+    public override double GetPace()                    // get pace
+    {
+        return 60 / _speed;
+    }
 
 }
