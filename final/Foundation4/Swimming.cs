21d0f0a12e70962d43196d13309ac409f010091e
Ben Quiñones <qbenji99@gmail.com>
2024-04-06 19:46:38 -0600

    Finished Swimming Class

--- final/Foundation4/Swimming.cs
+++ final/Foundation4/Swimming.cs
@@ -2,5 +2,27 @@ using System;
 
 public class Swimming : Activity
 {
+    private int _laps;
+
+    public Swimming(DateTime date, int durationMin, int laps)     // constructor
+        : base(date, durationMin)
+    {
+        _laps = laps;
+    }
+
+    public override double GetDistance()                // get distance
+    {
+        return _laps * 50 / 1000;
+    }
+
+    public override double GetSpeed()                   // get speed
+    {
+        return GetDistance() / GetDurationMin() * 60;
+    }
+
+    public override double GetPace()                    // get pace
+    {
+        return GetDurationMin() / GetDistance();
+    }
 
 }
